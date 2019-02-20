95d3d4c20fd0b099db73e40d7a90844fb87ff58d
Sean MacLaughlin <seanmaclaughlin@zipcoders-MBP.home>
2019-02-20 17:45:00 -0500

    Dicey Lab

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,27 @@
+import java.util.Random;
+
 public class Dice {
+
+    private Integer numberOfDice;
+
+    public Dice(Integer numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+
+    public Integer tossAndSum() {
+        Integer sum = 0;
+        for(int i = 0; i < numberOfDice; i++) {
+            sum += roll();
+
+
+        }
+        return sum;
+    }
+
+    private Integer roll() {
+        return (int) (Math.random() * 6 + 1);
+    }
+
 
 
 }
